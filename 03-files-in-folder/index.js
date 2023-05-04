129895fc77fee4e5abf66fa9f8b3d75dbc818879
avmochkina <anastasiaavmochkina@yandex.by>
2023-05-04 16:25:27 +0300

    feat: implement output in console files and get information about their size 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      if (stats.isFile()) {
+        const fileSizeInBytes = stats.size;
+        const fileSizeInKB = fileSizeInBytes / 1024;
+        const fileExtension = path.extname(file).substring(1);
+
+        console.log(`${path.parse(file).name} - ${fileExtension} - ${fileSizeInKB.toFixed(3)}kb`);
+      } else {
+        console.error(`Error: ${file} is not a file!`);
+      }
+    });
+  });
+});
+
+
+
 
